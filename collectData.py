278133d48bba27e3fc4f2fc8c5b7a237e1ac883c
Zoey VanderMeer <zoeyvanderm@gmail.com>
2024-12-03 00:09:33 -0500

    Add comments and update location/date processing

--- collectData.py
+++ collectData.py
@@ -15,14 +15,16 @@ from transformers import pipeline
 from twikit import Client, TooManyRequests
 import urllib
 
-filename = ""
+dbFilename = ""
 
+# load sentiment analysis AI
 mps_device = torch.device("mps")
 sentiment_pipeline = pipeline("text-classification", model="cardiffnlp/twitter-xlm-roberta-base-sentiment", device=mps_device)
 
 location_queue = []
 finished_obtaining_tweets = False
 
+# build a query from given searchData
 def buildQuery(searchData):
     query = ""
 
@@ -48,15 +50,12 @@ def buildQuery(searchData):
 
     query += "lang:en "
 
-    query += f"until:{searchData['dateEnd']} "
-
-    query += f"since:{searchData['dateStart']}"
-
     return query
 
 
+# Create new database
 def createDatabase():
-    connection = sqlite3.connect(filename)
+    connection = sqlite3.connect(dbFilename)
     cursor = connection.cursor()
 
     searchesTable = """ CREATE TABLE SEARCHES (
@@ -96,18 +95,19 @@ def createDatabase():
 
     connection.close()
 
-    print(f"Database \"{filename}\" has been created")
+    print(f"Database \"{dbFilename}\" has been created")
 
 
+# If this is a new query, create a new entry in database, otherwise return ID of previous query
 def getQueryId(query, searchData):
-    connection = sqlite3.connect(filename)
+    connection = sqlite3.connect(dbFilename)
     cursor = connection.cursor()
 
     queryId = cursor.execute("SELECT id FROM SEARCHES WHERE fullQuery = ?", (query,)).fetchone()
 
     if(queryId == None):
-        cursor.execute("INSERT INTO SEARCHES (anyTerms, exactTerms, exactPhrase, notTerms, dateStart, dateEnd, fullQuery) VALUES (?,?,?,?,?,?,?)", 
-                       (str(searchData["anyTerms"]), str(searchData["exactTerms"]), searchData["exactPhrase"], str(searchData["notTerms"]), searchData["dateStart"], searchData["dateEnd"], query))
+        cursor.execute("INSERT INTO SEARCHES (anyTerms, exactTerms, exactPhrase, notTerms, fullQuery) VALUES (?,?,?,?,?)", 
+                       (str(searchData["anyTerms"]), str(searchData["exactTerms"]), searchData["exactPhrase"], str(searchData["notTerms"]), query))
         
         id = cursor.lastrowid
 
@@ -119,11 +119,13 @@ def getQueryId(query, searchData):
     connection.close()
 
     return id
-        
 
+        
+# Scrape data from twitter
 async def scrape(query: str, minTweets: int, queryId: int):
     client = Client(language='en-US')
 
+    # If cookies.json exists, login using cookies, otherwise login using credentials and save cookies
     if os.path.isfile('cookies.json'):
         client.load_cookies('cookies.json')
     else:
@@ -139,63 +141,86 @@ async def scrape(query: str, minTweets: int, queryId: int):
 
         await client.login(auth_info_1=username, auth_info_2=email, password=password)
         client.save_cookies('cookies.json')
+    
+    for i in range(3):
+        tweet_count = 0
+        tweets = None
 
-    tweet_count = 0
-    tweets = None
+        if(i == 0):
+            print(f'{datetime.now()} - Processing 2010-2014')
+            newQuery = query + "until:2015-01-01 since:2010-01-01"
+        elif(i == 1):
+            print(f'{datetime.now()} - Processing 2015-2019')
+            newQuery = query + "until:2020-01-01 since:2015-01-01"
+        else:
+            print(f'{datetime.now()} - Processing 2020-2024')
+            newQuery = query + "until:2025-01-01 since:2020-01-01"
 
-    while tweet_count < minTweets:
-        try:
-            if tweets is None:
-                print(f'{datetime.now()} - Getting tweets...')
-                tweets = await client.search_tweet(query, product='Top')
-            else:
+        # Loop until the desired number of tweets has been reached
+        while tweet_count < minTweets:
+
+            # Searches tweets from twitter
+            try:
+                if tweets is None:
+                    print(f'{datetime.now()} - Getting tweets...')
+                    tweets = await client.search_tweet(newQuery, product='Top')
+                else:
+                    wait_time = randint(5, 10)
+                    print(f'{datetime.now()} - Getting next tweets after {wait_time} seconds...')
+                    time.sleep(wait_time)
+                    tweets = await tweets.next()
+
+            # If hit rate limit, wait until rate limit expires
+            except TooManyRequests as e:
+                rate_limit_reset = datetime.fromtimestamp(e.rate_limit_reset)
+                print(f'{datetime.now()} - Rate limit reached. Waiting until {rate_limit_reset}')
+                wait_time = rate_limit_reset - datetime.now()
+                time.sleep(wait_time.total_seconds())
+
                 wait_time = randint(5, 10)
-                print(f'{datetime.now()} - Getting next tweets after {wait_time} seconds...')
+                print(f'{datetime.now()} - Getting next tweets after {wait_time} seconds ...')
                 time.sleep(wait_time)
-                tweets = await tweets.next()
+                continue
 
-        except TooManyRequests as e:
-            rate_limit_reset = datetime.fromtimestamp(e.rate_limit_reset)
-            print(f'{datetime.now()} - Rate limit reached. Waiting until {rate_limit_reset}')
-            wait_time = rate_limit_reset - datetime.now()
-            time.sleep(wait_time.total_seconds())
+            if not tweets:
+                print(f'{datetime.now()} - No more tweets found')
+                break
 
-            wait_time = randint(5, 10)
-            print(f'{datetime.now()} - Getting next tweets after {wait_time} seconds ...')
-            time.sleep(wait_time)
-            continue
+            tweet_count += len(tweets)
 
-        if not tweets:
-            print(f'{datetime.now()} - No more tweets found')
-            break
+            # Start thread to process received batch of tweets
+            processBatchThread = threading.Thread(target=processBatch, args=(tweets, queryId))
+            processBatchThread.start()
 
-        tweet_count += len(tweets)
+            print(f'{datetime.now()} - Got {len(tweets)} new tweets')
 
-        processBatchThread = threading.Thread(target=processBatch, args=(tweets, queryId))
-        processBatchThread.start()
+        print(f'{datetime.now()} - Done! Got {tweet_count} tweets total')
 
-        print(f'{datetime.now()} - Got {len(tweets)} new tweets')
-
-    print(f'{datetime.now()} - Done! Got {tweet_count} tweets total')
-
+    # Indicate all tweets have been fetched
     global finished_obtaining_tweets
     finished_obtaining_tweets = True
 
+
+# Process a batch of tweets
 def processBatch(tweets, queryId):
-    connection = sqlite3.connect(filename)
+    # Create new connection to the database
+    connection = sqlite3.connect(dbFilename)
     cursor = connection.cursor()
 
     fullTweetDataList = []
 
     for tweet in tweets:
+        # Ignore any accounts explicitly labelled "parody"
         if "parody" in tweet.user.name.lower():
             continue
 
+        # Ignore if tweet is not in supported sentiment analysis languages
         if tweet.lang.lower() not in ["ar", "en", "fr", "de", "hi", "it", "sp", "pt"]:
             continue
 
         date_format_str = "%a %b %d %H:%M:%S %z %Y"
 
+        # Configure dictionary of tweet data
         myTweetDict = {
             "queryId": queryId,
             "tweetId": tweet.id,
@@ -208,54 +233,77 @@ def processBatch(tweets, queryId):
             "likes": tweet.favorite_count
         }
 
+        # Insert tweet into database
         cursor.execute("INSERT INTO TWEETS (queryId, tweetId, language, username, originalLocation, content, creationDate, retweets, likes) VALUES (?,?,?,?,?,?,?,?,?)", tuple(myTweetDict.values()))
 
         myTweetDict["id"] = cursor.lastrowid
 
+        # Add tweet to list for sentiment analysis
         fullTweetDataList.append(myTweetDict)
 
+        # Add tweet to location queue
         location_queue.append({
             "id": cursor.lastrowid,
             "location": tweet.user.location
         })
 
+    # Commit changes to the database
     connection.commit()
 
+    # Get content of all tweets in batch
     tweetContent = [d['content'] for d in fullTweetDataList]
 
+    # Process sentiment for all tweets in batch
     sentiment_data = sentiment_pipeline(tweetContent)
 
+    # Update values in each tweet dictionary
     for i in range (len(tweetContent)):
         fullTweetDataList[i]["sentiment"] = sentiment_data[i]["label"]
         fullTweetDataList[i]["confidence"] = sentiment_data[i]["score"]
 
+    # Update data in database
     for tweet in fullTweetDataList:
         cursor.execute("UPDATE TWEETS SET sentiment = ?, confidence = ? WHERE id = ?", (tweet["sentiment"], tweet["confidence"], tweet["id"]))
 
+    # Commit changes to database and close the connection
     connection.commit()
-
     connection.close()
 
+
+# Process location for each tweet
 def processLocation():
-    connection = sqlite3.connect(filename)
+    # Connect to the database
+    connection = sqlite3.connect(dbFilename)
     cursor = connection.cursor()
 
+    # Base url for the request
+    baseUrl = "https://nominatim.openstreetmap.org/search?format=json&accept-language=en&addressdetails=1&limit=1&q="
+
+    # Loop until all tweets have been processed
     while(not finished_obtaining_tweets or len(location_queue) > 0):
+        # If there is an item on the queue, remove it and start processing
         if(len(location_queue) > 0):
             myItem = location_queue.pop(0)
 
+            # Process if the location is not empty
             if(myItem['location'] != ""):
+                print(f'{datetime.now()} - Processing location for \"{myItem["location"]}\"')
 
-                print(f"{datetime.now()} - Processing location for \"{myItem['location']}\"")
+                headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
 
-                baseUrl = "https://nominatim.openstreetmap.org/search?q="
+                parsedLocation = urllib.parse.quote_plus(myItem['location'], safe='')
 
-                parameters = urllib.parse.quote_plus(myItem['location'], safe='')
-
-                locationData = json.loads(requests.get(baseUrl+parameters+"&format=json&accept-language=en").content)
+                locationData = json.loads(requests.get(baseUrl+parsedLocation, headers=headers).content)
 
                 if(locationData != []):
-                    cursor.execute("UPDATE TWEETS SET locationLatitude = ?, locationLongitude = ?, validLocation = 1 WHERE id = ?", (locationData[0]["lat"], locationData[0]["lon"], myItem["id"]))
+                    locationData = locationData[0]
+
+                    if not 'ISO3166-2-lvl4' in locationData['address'].keys():
+                        locationData['address']['ISO3166-2-lvl4'] = ""
+
+                    cursor.execute(
+                        "UPDATE TWEETS SET locationLatitude = ?, locationLongitude = ?, countryCode = ?, stateCode = ?, validLocation = 1 WHERE id = ?", 
+                        (locationData["lat"], locationData["lon"], locationData["address"]["country_code"], locationData["address"]["ISO3166-2-lvl4"], myItem['id']))
 
                     connection.commit()
                 else:
@@ -263,21 +311,33 @@ def processLocation():
 
                     connection.commit()
 
+            # Wait 1 second to ensure API is not overused
             time.sleep(1.1)
 
     connection.close()
     
+
 if __name__ == "__main__":
+    # make sure database filename has been passed as CLI argument
     try:
         if sys.argv[1] == "":
-            print("Please enter a filename")
+            print("Please enter a filename for the database")
             sys.exit()
     except IndexError:
-        print("Please enter a filename")
+        print("Please enter a filename for the database")
         sys.exit()
 
-    filename = sys.argv[1]
+    dbFilename = sys.argv[1]
 
+    # start thread to process location
+    try:
+        if sys.argv[2].lower() == "true":
+            processLocationThread = threading.Thread(target=processLocation, args=())
+            processLocationThread.start()
+    except IndexError:
+        pass
+
+    # check for required files
     if not os.path.isfile("search.json"):
         print("search.json not found")
         sys.exit()
@@ -285,14 +345,14 @@ if __name__ == "__main__":
     with open('search.json') as f:
         searchData = json.load(f)
 
-    if not os.path.isfile(filename):
+    if not os.path.isfile(dbFilename):
         createDatabase()
 
+    # build query from search.json
     query = buildQuery(searchData)
     
+    # check if query has already been made
     queryId = getQueryId(query, searchData)
 
-    # processLocationThread = threading.Thread(target=processLocation, args=())
-    # processLocationThread.start()
-
+    # start scraping data
     asyncio.run(scrape(query, searchData["minTweets"], queryId))
